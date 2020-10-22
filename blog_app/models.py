4bfd0c80a6f979c05aff855f88fec97a0abaa072
EmmaMKB <emmanuelmakabu7@gmail.com>
2020-10-22 12:53:59 -0700

    ✨ Add BlogPost model

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,1 +1,12 @@
 from django.db import models
+from django.utils import timezone
+import uuid
+# Create your models here.
+
+
+class BlogPost:
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255, default="")
+    content = models.CharField(max_length=4096, default="<h1>No content Yet</h1>")
+    date_posted = models.DateTimeField(default=timezone.now)
